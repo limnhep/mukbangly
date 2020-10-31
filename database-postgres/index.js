6d9419520c512fe271faab8c457d95cb263ce8fd
Lim Nhep <limenhep@gmail.com>
2020-10-30 20:49:04 -0700

    Initialize npm install and set-up Postgres Schema file

---
+++ database-postgres/index.js
@@ -0,0 +1,15 @@
+const { Client } = require('pg')
+const client = new Client({
+  host: 'localhost',
+  user: 'mvp_user',
+  password: 'hackreactor',
+  database: 'live_streaming'
+})
+
+const client = new Client()
+client
+  .connect()
+  .then(() => console.log('PostgreSQL is now connected'))
+  .catch(err => console.error('connection error', err.stack))
+
+module.exports = client;
