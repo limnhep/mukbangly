4d79e9009186971934b47b23c5e43d043ebaf84a
Lim Nhep <limenhep@gmail.com>
2020-10-31 11:00:35 -0700

    Render React

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import axios from 'axios';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('live-streaming')
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      feeds: []
+    }
+  }
+
+  requestFeeds() {
+    axios.get('/getFeeds')
+      .then(response => {
+        this.setState({
+          feeds: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error)
+      });
+  }
+
+  componentDidMount() {
+    this.requestFeeds();
+  }
+
+  render() {
+    return (
+      <div>
+        Hello from React!
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('live-streaming'));
